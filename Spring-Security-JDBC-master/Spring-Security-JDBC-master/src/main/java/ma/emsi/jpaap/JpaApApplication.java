c5fa92e737963d00ccbe8ea7d1d471af9abe3342
Mounaha12 <104575628+Mounaha12@users.noreply.github.com>
2022-04-28 15:52:04 +0200

    Add files via upload

---
+++ Spring-Security-JDBC-master/Spring-Security-JDBC-master/src/main/java/ma/emsi/jpaap/JpaApApplication.java
@@ -0,0 +1,61 @@
+package ma.emsi.jpaap;
+
+import ma.emsi.jpaap.entities.Patient;
+import ma.emsi.jpaap.repositories.PatientRepository;
+import ma.emsi.jpaap.sec.service.SecurityService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class JpaApApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApApplication.class, args);
+    }
+
+
+    @Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository){
+        return args -> {
+
+            patientRepository.save(new Patient(null,"Hassan",new Date(),false,22));
+            patientRepository.save(new Patient(null,"Mouad",new Date(),false,22));
+            patientRepository.save(new Patient(null,"Nassim",new Date(),false,22));
+            patientRepository.save(new Patient(null,"Nada",new Date(),false,22));
+            patientRepository.save(new Patient(null,"Yassine",new Date(),false,22));
+            patientRepository.save(new Patient(null,"Hmed",new Date(),false,22));
+            patientRepository.save(new Patient(null,"Zaid",new Date(),false,22));
+            patientRepository.save(new Patient(null,"Zineb",new Date(),false,22));
+            patientRepository.save(new Patient(null,"Omar",new Date(),false,22));
+            patientRepository.save(new Patient(null,"Youssef",new Date(),false,22));
+            patientRepository.save(new Patient(null,"YYY",new Date(),false,22));
+            patientRepository.save(new Patient(null,"DDDD",new Date(),false,22));
+            patientRepository.findAll().forEach(p -> {
+                System.out.println(p.getNom());
+            });
+
+        };
+    }
+   // @Bean
+    CommandLineRunner saveUsers(SecurityService securityService){
+        return args -> {
+            securityService.saveNewUser("mouna","1234","1234");
+            securityService.saveNewUser("mohammed","1234","1234");
+            securityService.saveNewUser("yasmine","1234","1234");
+            securityService.saveNewRole("USER","");
+            securityService.saveNewRole("ADMIN","");
+            securityService.addRoleToUser("mouna","ADMIN");
+            securityService.addRoleToUser("mouna","USER");
+            securityService.addRoleToUser("mohammed","USER");
+            securityService.addRoleToUser("yasmine","USER");
+
+
+
+        };
+    }
+
+}
